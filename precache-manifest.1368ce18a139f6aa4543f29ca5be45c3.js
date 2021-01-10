f6838547d0932ec5137d7ee5f80a5b86929fec45
Marah <Marah_rayan@yahoo.com>
2021-01-10 23:20:13 +0000

    Updates

---
+++ precache-manifest.1368ce18a139f6aa4543f29ca5be45c3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84ed2ecb2fc3783d3df1f9d37d5617b",
+    "url": "/Final-Project/index.html"
+  },
+  {
+    "revision": "f02c9146a291a170e802",
+    "url": "/Final-Project/static/css/main.2c020fea.chunk.css"
+  },
+  {
+    "revision": "d1e14d898b4305ed842b",
+    "url": "/Final-Project/static/js/2.479cffea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Final-Project/static/js/2.479cffea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02c9146a291a170e802",
+    "url": "/Final-Project/static/js/main.9fe547cb.chunk.js"
+  },
+  {
+    "revision": "c0d3db3f089a847f4de0",
+    "url": "/Final-Project/static/js/runtime-main.6e25d906.js"
+  },
+  {
+    "revision": "05c8cc112ff5cb66e6cea2b8d6e4b540",
+    "url": "/Final-Project/static/media/John.05c8cc11.jpg"
+  },
+  {
+    "revision": "f763da7f2bdea26d4fcfc0002c42f2f1",
+    "url": "/Final-Project/static/media/Kate.f763da7f.jpg"
+  },
+  {
+    "revision": "02d16a149016e73388a58549212dfd23",
+    "url": "/Final-Project/static/media/Sam.02d16a14.jpg"
+  },
+  {
+    "revision": "4e341538079f493d59be2d4365478a4c",
+    "url": "/Final-Project/static/media/Sarah.4e341538.jpg"
+  },
+  {
+    "revision": "bca49285b472285b2cabaec0ba4ebcb0",
+    "url": "/Final-Project/static/media/art.bca49285.png"
+  },
+  {
+    "revision": "4fcf94b8c76cd1f1fdf9f9b30262c3cb",
+    "url": "/Final-Project/static/media/asian.4fcf94b8.jpg"
+  },
+  {
+    "revision": "c2146b777a5873988a9b9850703e4239",
+    "url": "/Final-Project/static/media/blue.c2146b77.jpg"
+  },
+  {
+    "revision": "bb926d9cc3ee7503648740f09a4aab87",
+    "url": "/Final-Project/static/media/forest.bb926d9c.png"
+  },
+  {
+    "revision": "0e1d1f0e6836a50b03fd642e5cbe1dde",
+    "url": "/Final-Project/static/media/hijabi.0e1d1f0e.jpg"
+  },
+  {
+    "revision": "ac6749b0551331e1652a2c9c5e5a1b4f",
+    "url": "/Final-Project/static/media/image-2.ac6749b0.jpg"
+  },
+  {
+    "revision": "48f82304823809bba6b35236a38ec28c",
+    "url": "/Final-Project/static/media/landscape1.48f82304.jpg"
+  },
+  {
+    "revision": "600254092a52743616220618589b5dcf",
+    "url": "/Final-Project/static/media/landscape2.60025409.jpg"
+  },
+  {
+    "revision": "856b391f3ea5a4a0125c8f3d3fb254bb",
+    "url": "/Final-Project/static/media/landscape3.856b391f.jpg"
+  },
+  {
+    "revision": "be7c7b71f850e0b476bc512a007250d9",
+    "url": "/Final-Project/static/media/landscape4.be7c7b71.jpg"
+  },
+  {
+    "revision": "48eaa866a9c7e297f71cfaa52fe94f61",
+    "url": "/Final-Project/static/media/landscape5.48eaa866.jpg"
+  },
+  {
+    "revision": "f0502f0b2e84a35e5ec5d49a52445be6",
+    "url": "/Final-Project/static/media/livingroom.f0502f0b.jpg"
+  },
+  {
+    "revision": "0eb108c7a75e5723884e4959088381aa",
+    "url": "/Final-Project/static/media/man2.0eb108c7.jpg"
+  },
+  {
+    "revision": "12e6e680876c623a26327b22057949e6",
+    "url": "/Final-Project/static/media/mansion.12e6e680.jpg"
+  },
+  {
+    "revision": "5a89f6308254ec6ecd1c41989c6acfc1",
+    "url": "/Final-Project/static/media/property.5a89f630.png"
+  },
+  {
+    "revision": "f03a1f6897feb096c89b7a5bd44dab44",
+    "url": "/Final-Project/static/media/subway.f03a1f68.jpg"
+  }
+]);
